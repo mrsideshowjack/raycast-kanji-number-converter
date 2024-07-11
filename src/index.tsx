70178967e3572ec2150b9323cc21575136fb18d0
Jack Mason <jack.mason@kraken.tech>
2024-07-11 15:13:58 +0900

    prepare for publish

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,22 @@ import { kanji2number } from "@geolonia/japanese-numeral";
 
 let md: string;
 let num: number;
+let numComma: string;
 
 export default function Command({ arguments: { jpNumber } }: LaunchProps<{ arguments: Arguments.Index }>) {
   try {
-    num = kanji2number(jpNumber.replace(',', ''));
-    md = `Converted ${jpNumber} into ${num}`;
+    num = kanji2number(jpNumber.replace(",", ""));
+    numComma = num.toLocaleString();
+    md = `
+## Input
+
+${jpNumber} 
+
+## Output
+
+${numComma}`;
   } catch (error) {
-    const errMsg = `Error converting ${jpNumber}. Please ensure it's a valid Japanese numeral.`;
+    const errMsg = `Error converting ${jpNumber}. Please ensure it's a valid Japanese numeral. eg 一、二、三, 430万, `;
     md = errMsg;
     showToast({
       style: Toast.Style.Failure,
@@ -23,11 +32,10 @@ export default function Command({ arguments: { jpNumber } }: LaunchProps<{ argum
       markdown={md}
       actions={
         <ActionPanel>
+          <Action.CopyToClipboard content={numComma} title={`Copy ${numComma} to Clipboard`} />
           <Action.CopyToClipboard content={num} title={`Copy ${num} to Clipboard`} />
         </ActionPanel>
       }
     />
   );
 }
-
-// 430万
